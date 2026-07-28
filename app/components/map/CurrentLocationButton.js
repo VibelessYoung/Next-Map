726b28e09a41b4da8e468c60329eb8eaa6971c31
Amir <vibelessyoung@gmail.com>
2026-07-28 19:01:21 +0330

    CurrentLocationButton created

---
+++ app/components/map/CurrentLocationButton.js
@@ -0,0 +1,35 @@
+"use client";
+
+import { LocateFixed, LoaderCircle } from "lucide-react";
+
+export default function CurrentLocationButton({ loading, onClick }) {
+  return (
+    <button
+      onClick={onClick}
+      disabled={loading}
+      className="
+        absolute
+        bottom-6
+        right-6
+        z-[1000]
+        flex
+        h-14
+        w-14
+        items-center
+        justify-center
+        rounded-full
+        bg-white
+        shadow-xl
+        transition
+        hover:scale-105
+        disabled:cursor-not-allowed
+      "
+    >
+      {loading ? (
+        <LoaderCircle className="h-6 w-6 animate-spin" />
+      ) : (
+        <LocateFixed className="h-6 w-6" />
+      )}
+    </button>
+  );
+}
