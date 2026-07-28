d209d0626d8b0a791c077739ba2b6bafa4292374
Amir <vibelessyoung@gmail.com>
2026-07-28 19:07:26 +0330

    install lucide-react

--- app/components/Map.js
+++ app/components/Map.js
@@ -1,8 +1,28 @@
 "use client";
 
 import { MapContainer, TileLayer } from "react-leaflet";
+import { useState } from "react";
+import CurrentLocationButton from "./map/CurrentLocationButton"
 
 export default function GlobalMap() {
+  const [loading, setLoading] = useState(false);
+
+  const handleCurrentLocation = () => {
+    setLoading(true);
+
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        console.log(position.coords.latitude);
+        console.log(position.coords.longitude);
+
+        setLoading(false);
+      },
+      (error) => {
+        console.error(error);
+        setLoading(false);
+      },
+    );
+  };
   return (
     <MapContainer
       center={[35.6892, 51.389]}
@@ -12,6 +32,10 @@ export default function GlobalMap() {
       <TileLayer
         attribution="&copy; OpenStreetMap contributors"
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      <CurrentLocationButton
+        loading={loading}
+        onClick={handleCurrentLocation}
       />
     </MapContainer>
   );
